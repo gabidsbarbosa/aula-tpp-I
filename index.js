76755d5fe7cb5a87964ff43d7de3b17d5072bc89
Gabriela Barbosa <gabrielasbarbos@gmail.com>
2023-03-01 11:52:26 -0300

    corrigindo erros

--- index.js
+++ index.js
@@ -14,16 +14,16 @@ const multiplicar = (valor1, valor2) => {
     console.log(`resultado: ${valor1 * valor2}`)
 }
 
-exports.calculo = multiplicar
+exports.mult = multiplicar
 
 const subtrair = (valor1, valor2) => {
     console.log(`resultado: ${valor1 - valor2}`)
 }
 
-exports.calculo = subtrair
+exports.sub = subtrair
 
 const dividir = (valor1, valor2) => {
     console.log(`resultado: ${valor1 / valor2}`)
 }
 
-exports.calculo = dividir
+exports.div = dividir
